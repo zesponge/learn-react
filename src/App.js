be695cccceee81736157f384089d51038796154a
mario <sumario82@gmail.com>
2022-01-05 22:07:18 -0500

    about 13 minutes in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, { useState } from 'react';
+import TodoList from "./TodoList";
 
 function App() {
-  return null
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  return (
+    <>
+    <TodoList todos={todos} />
+    <input type="text"/>
+    <button>add Todo</button>
+    <button>Clear Completed</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
